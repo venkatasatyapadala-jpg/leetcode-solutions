4199e693e10d8a3b776165489a29701fb7a33f53
Padala Venkata satya <venkatasatyapadala@gmail.com>
2026-09-04 21:44:28 +0530

    Add Easy Python Solution for 4284. Smallest Stable Index I from LeetCode

---
+++ LeetCode/Python/Easy/4284. Smallest Stable Index I/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def firstStableIndex(self, nums, k):
+        n = len(nums)
+
+        suffix_min = [0] * n
+        suffix_min[n - 1] = nums[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(nums[i], suffix_min[i + 1])
+
+        prefix_max = nums[0]
+
+        for i in range(n):
+            prefix_max = max(prefix_max, nums[i])
+
+            if prefix_max - suffix_min[i] <= k:
+                return i
+
+        return -1
