4c9361baf04fa8490b466bb34e397201c178d861
Padala Venkata satya <venkatasatyapadala@gmail.com>
2026-09-05 11:47:34 +0530

    Add Medium Python Solution for 3904. Smallest Stable Index II from LeetCode

---
+++ LeetCode/Python/Medium/3904. Smallest Stable Index II/Solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def firstStableIndex(self, nums, k):
+        n = len(nums)
+
+        # Suffix minimum
+        suffix_min = [0] * n
+        suffix_min[n - 1] = nums[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(nums[i], suffix_min[i + 1])
+
+        # Prefix maximum
+        prefix_max = nums[0]
+
+        for i in range(n):
+            prefix_max = max(prefix_max, nums[i])
+
+            if prefix_max - suffix_min[i] <= k:
+                return i
+
+        return -1
